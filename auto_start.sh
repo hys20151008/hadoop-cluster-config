f950317134453289a528e2d96425c2dfc1007765
HYS <hysyeah@126.com>
2018-02-27 11:13:23 +0800

    spark use mysql store metadata(add hive-site.xml)

--- auto_start.sh
+++ auto_start.sh
@@ -2,6 +2,7 @@
 chown root:root -R /root/.ssh
 chmod -R 700 /root/.ssh
 /etc/init.d/ssh start
-#/opt/hadoop-2.7.4/sbin/start-all.sh
-#/opt/spark-2.2.0-bin-hadoop2.7/sbin/start-all.sh
+/opt/hadoop-2.7.4/sbin/start-all.sh
+/opt/hadoop-2.7.4/sbin/hadoop-daemons.sh start secondarynamenode
+/opt/spark-2.2.0-bin-hadoop2.7/sbin/start-all.sh
 /bin/bash
